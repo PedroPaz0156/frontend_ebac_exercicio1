01ecca6d5180d43bf67f2ee35db5fe93224a2719
PedroPaz0156 <n00b56.ppz@gmail.com>
2025-04-10 15:56:20 -0300

    Modified 3 files

---
+++ main.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("formulario");
+const messagemSucesso = document.querySelector(".success-message");
+const messagemErro = document.querySelector(".error-message");
+
+form.addEventListener("submit", function(e) {
+    let valor1 = document.getElementById("campoA");
+    let valor2 = document.getElementById("campoB");
+
+    if(valor1.value < valor2.value) {
+        messagemSucesso.value = "Comparação concluída, o ${valor1} é menor do que ${valor2}." 
+        messagemSucesso.style.display = "block";
+    }else {
+        messagemErro.style.display = "block";
+    }
+
+    e.preventDefault();
+})
