6cfa319e90ffbfea638347080fdc1ffcdd0d2485
PedroPaz0156 <n00b56.ppz@gmail.com>
2025-04-19 00:05:06 -0300

    Fixed CSS and JS

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const form = document.getElementById("formulario");
-const messagemSucesso = document.querySelector(".success-message");
-const messagemErro = document.querySelector(".error-message");
+const messagemSucesso = document.querySelector("p.success-message");
+const messagemErro = document.querySelector("p.error-message");
 
 form.addEventListener("submit", function(e) {
     let valor1 = document.getElementById("campoA");
     let valor2 = document.getElementById("campoB");
 
     if(valor1.value < valor2.value) {
-        messagemSucesso.value = "Comparação concluída, o ${valor1} é menor do que ${valor2}." 
+        messagemSucesso.value = "Comparação concluída, o ${valor1} é menor do que ${valor2}.";
         messagemSucesso.style.display = "block";
     }else {
         messagemErro.style.display = "block";
