91bfbddeab237c4e69b63c953c0bff0340952570
Pedro Paz <127502768+PedroPaz0156@users.noreply.github.com>
2025-09-29 16:22:22 -0300

    commit inicial

---
+++ atividade.ts
@@ -0,0 +1,12 @@
+function multiplicacao(valor1: number, valor2:number) { 
+    const produto = valor1 * valor2;
+    return produto;
+}
+
+
+function olaNome(nome: String) { 
+    console.log("Olá " + nome);
+}
+
+multiplicacao(5, 25);
+olaNome("Pedro");
